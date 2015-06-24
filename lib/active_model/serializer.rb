5de0b2a7ef143c82a2ef64268a5c2feeaf09a91d
briandavidwetzel <briandavidwetzel@gmail.com>
2015-06-24 16:56:58 -0400

    unfix and fix fix

--- lib/active_model/serializer.rb
+++ lib/active_model/serializer.rb
@@ -162,7 +162,7 @@ end
     end
 
     def associations
-      associations = @object.nil? ? {} : self.class._associations
+      associations = self.class._associations
       included_associations = filter(associations.keys)
       associations.each_with_object({}) do |(name, association), hash|
         if included_associations.include? name
@@ -195,7 +195,7 @@ end
     end
 
     def embedded_in_root_associations
-      associations = self.class._associations
+      associations = @object.nil? ? {} : self.class._associations
       included_associations = filter(associations.keys)
       associations.each_with_object({}) do |(name, association), hash|
         if included_associations.include? name
